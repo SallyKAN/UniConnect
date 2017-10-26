b2d8240da34cb1ebea7b9f310463193fabb9c5fe
John <jodu0512@uni.sydney.edu.au>
2017-10-26 12:05:07 +1100

    form

--- uniconnect_app/forms.py
+++ uniconnect_app/forms.py
@@ -21,3 +21,8 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class ResetPasswordForm(forms.Form):
+    email = forms.EmailField(max_length=150, help_text='Required')
+
